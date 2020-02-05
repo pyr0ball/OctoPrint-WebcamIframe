9575eee19e30a5f191d4ca880555cdc145347759
pyr0ball <pyroballpcs@gmail.com>
2020-02-05 08:09:15 -0800

    trying out a different hls script

--- octoprint_webcam_iframe/static/js/webcam_iframe.js
+++ octoprint_webcam_iframe/static/js/webcam_iframe.js
@@ -12,7 +12,8 @@ $(function() {
 
 		self.onAllBound = function(allViewModels){
 			var webcamurl = self.settingsViewModel.webcam_streamUrl();
-			$('#webcam_container').replaceWith('<video width="588" controls autoplay><source  src="' + webcamurl + '" type="application/x-mpegURL"></video>');
+//			$('#webcam_container').replaceWith('<video width="588" controls autoplay><source  src="' + webcamurl + '" type="application/x-mpegURL"></video>');
+			$('#webcam_container').replaceWith('<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script><video width=588 id="video"></video><script>;if(Hls.isSupported()){var video = document.getElementById("video");var hls = new Hls();hls.loadSource("' + webcamurl + '"); hls.attachMedia(video);hls.on(Hls.Events.MANIFEST_PARSED,function() { video.play();});}</script>');
 		}
 	}
 
